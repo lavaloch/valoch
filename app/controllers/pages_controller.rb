c3dba7828484c96591511ba4c96e98ee3e31a9d4
Valentin Selve <val.selve@gmail.com>
2018-08-15 16:01:56 +0200

    Ajout de project manager, de alternance et freelance

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!, only: [:home, :about, :project_manager, :alternance, :freelance]
 
   def home
   end
 
   def about
+  end
+
+  def project_manager
+  end
+
+  def alternance
+  end
+
+  def freelance
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/me', to: 'pages#about', as: 'about'
+  get '/project-manager', to: 'pages#project_manager', as: 'pm'
+  get '/project-manager/alternance', to: 'pages#alternance', as: 'alternance'
+  get '/project-manager/mktg-midas', to: 'pages#freelance', as: 'freelance'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
